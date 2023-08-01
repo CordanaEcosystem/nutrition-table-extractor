142990d938a30e4863391537ccffee1796a81e6c
Dhanapandi M <dhanapandi.m007@gmail.com>
2023-08-01 15:43:19 +0530

    adds error and exception handling

--- api/views.py
+++ api/views.py
@@ -25,127 +25,150 @@ sys.path.append(os.path.join(os.getcwd(), 'nutrition_extractor/data'))
 @csrf_exempt
 def nutritionExtract(request):
     if request.method == 'POST':
-        new_file = UploadFile(file=request.FILES['image'])
-        new_file.save()
-        file_path = new_file.file.path
-        name = new_file.file.name
-        response = detect_all(name, True)
-        os.remove(file_path)
-        file_path = new_file.file.path
+        try:
+            if 'image' not in request.FILES:
+                return JsonResponse({"error": "No 'image' file provided."}, status=400)
 
-        return JsonResponse(response)
+            new_file = UploadFile(file=request.FILES['image'])
+            new_file.save()
+            file_path = new_file.file.path
+            name = new_file.file.name
+            response = detect_all(name, True)
+            os.remove(file_path)
+
+            return JsonResponse(response)
+
+        except FileNotFoundError as e:
+            return JsonResponse({"error": f"FileNotFoundError: {e}"}, status=500)
+
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
 
 
 @csrf_exempt
 def ingredientExtract(request):
     if request.method == 'POST':
-        new_file = UploadFile(file=request.FILES['image'])
-        new_file.save()
-        # Delete the file from the server
+        try:
+            if 'image' not in request.FILES:
+                return JsonResponse({"error": "No 'image' file provided."}, status=400)
 
-        # extract_ingredients_from_image(request.FILES['image'])
-        name = new_file.file.name
-        file_path = new_file.file.path
-        with io.open(file_path, 'rb') as img:
+            new_file = UploadFile(file=request.FILES['image'])
+            new_file.save()
 
-            file_data = img.read()
-            file_base64 = base64.b64encode(file_data).decode('utf-8')
+            name = new_file.file.name
+            file_path = new_file.file.path
 
-            # Convert the file to base64
+            with io.open(file_path, 'rb') as img:
+                file_data = img.read()
+                file_base64 = base64.b64encode(file_data).decode('utf-8')
 
-            response = extract_ingredients_from_image(file_base64)
-            dict = {"ingredients": response}
+                response = extract_ingredients_from_image(file_base64)
+                dict_response = {"ingredients": response}
 
             os.remove(file_path)
 
-            return JsonResponse(dict)
+            return JsonResponse(dict_response)
+
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
 
 
 @csrf_exempt
 def process_new_product(request):
     if request.method == 'POST':
-        csv_filename = "api/Dataset analysis.csv"
-        all_nutrients = []
-        all_nutrients_mapping = {}
-        unit_mapping = {}
-        print("===", request)
-        body_unicode = request.body.decode('utf-8')
-        body = json.loads(body_unicode)
+        try:
+            
+            csv_filename = "api/Dataset analysis.csv"
+            all_nutrients = []
+            all_nutrients_mapping = {}
+            unit_mapping = {}
 
-        data = body["data"]
-        nutrients = {"macros": {}, "vitamins": {}, "minerals": {}}
-        with open(csv_filename) as f:
-            reader = csv.DictReader(f)
+            body_unicode = request.body.decode('utf-8')
+            body = json.loads(body_unicode)
 
-            for row in reader:
-                name = row["name"].strip().lower()
-                all_nutrients.append(name)
-                all_nutrients_mapping[name] = {
-                    "name": name,
-                    "new_name": row["new_name"].strip(),
-                    "category": row["category"].strip(),
-                    # "new_unit": row["new_unit"],
-                }
-                unit_mapping[row["new_name"].strip()] = {
-                    "category": row["category"].strip(),
-                    # "new_unit": row["new_unit"],
-                }
-        print(all_nutrients_mapping)
-        for record in data["nutrients"]:
-            result = all_nutrients_mapping.get(
-                record["name"].lower())
-            print("11",result)
-            key = record["name"].strip()
+            data = body["data"]
+            nutrients = {"macros": {}, "vitamins": {}, "minerals": {}}
+            with open(csv_filename) as f:
+                reader = csv.DictReader(f)
 
-            if result is not None:
-                value = record["amount"]
+                for row in reader:
+                    name = row["name"].strip().lower()
+                    all_nutrients.append(name)
+                    all_nutrients_mapping[name] = {
+                        "name": name,
+                        "new_name": row["new_name"].strip(),
+                        "category": row["category"].strip(),
+                        # "new_unit": row["new_unit"],
+                    }
+                    unit_mapping[row["new_name"].strip()] = {
+                        "category": row["category"].strip(),
+                        # "new_unit": row["new_unit"],
+                    }
 
-                print("222",value)
-                if(record["unitName"].strip()=="%" and Dv_values.get(result["new_name"])):
-                    value = Dv_values.get(result["new_name"]).get("amount")*value/100
-                    nutrients[result["category"]][result["new_name"]] = {
+            for record in data["nutrients"]:
+                result = all_nutrients_mapping.get(record["name"].lower())
+                key = record["name"].strip()
+
+                if result is not None:
+                    value = record["amount"]
+
+                    if (
+                        record["unitName"].strip() == "%"
+                        and Dv_values.get(result["new_name"])
+                    ):
+                        value = (
+                            Dv_values.get(result["new_name"]).get("amount")
+                            * value
+                            / 100
+                        )
+                        nutrients[result["category"]][result["new_name"]] = {
                             "value": value,
-                            "unit":  Dv_values.get(result["new_name"]).get("unit")
+                            "unit": Dv_values.get(result["new_name"]).get("unit"),
                         }
-                    # print("<<<<<",Dv_values.get(result["new_name"]).get("unit"))
-                else:
-                 nutrients[result["category"]][result["new_name"]] = {
+                    else:
+                        nutrients[result["category"]][result["new_name"]] = {
                             "value": value,
-                            "unit": record["unitName"]
+                            "unit": record["unitName"],
                         }
-              
-        print("-----", nutrients)
-        top_three = get_top_three(
-                    nutrients["vitamins"] | nutrients["minerals"])
-        # generate a random 6-digit number between 100000 and 999999 (inclusive)
-        random_number = random.randint(100000, 999999)
 
-        print("-9|",random_number)
-        new_data = {
+            top_three = get_top_three(nutrients["vitamins"] | nutrients["minerals"])
+            # generate a random 6-digit number between 100000 and 999999 (inclusive)
+            random_number = random.randint(100000, 999999)
 
-            "dataType": "Branded",
-                        "description": data["description"],
-                        "brandOwner": data["brandOwner"],
-                        "gtinUpc": data["gtinUpc"],
-                        "nutrients": str(json.dumps(nutrients)),
-                        "brandName": data["brandName"],
-                        "addedBy":data["uid"],
+            new_data = {
+                "dataType": "Branded",
+                "description": data["description"],
+                "brandOwner": data["brandOwner"],
+                "gtinUpc": data["gtinUpc"],
+                "nutrients": str(json.dumps(nutrients)),
+                "brandName": data["brandName"],
+                "addedBy": data["uid"],
+                "topThree": top_three,
+                "descriptionLength": len(data["description"]),
+                "fdcId": data["uid"] + "-" + str(random_number),
+            }
+            if "servingSize" in data:
+                new_data["servingSize"] = data["servingSize"]
+            if "servingSizeUnit" in data:
+                new_data["servingSizeUnit"] = data["servingSizeUnit"]
+            if "packageWeight" in data:
+                new_data["packageWeight"] = data["packageWeight"]
+            if "ingredients" in data:
+                new_data["allergyInformation"] = check_allergies(data["ingredients"])
+                new_data["ingredients"] = data["ingredients"]
 
-                        "topThree": top_three,
-                        "descriptionLength": len(data["description"]),
-                        "fdcId":data["uid"]+"-"+str(random_number)
-        }
-        if "servingSize" in data:
-            new_data["servingSize"] = data["servingSize"]
-        if "servingSizeUnit" in data:
-            new_data["servingSizeUnit"] = data["servingSizeUnit"]
-        if "packageWeight" in data:
-            new_data["packageWeight"] = data["packageWeight"]
-        if "ingredients" in data:
-            new_data["allergyInformation"] = check_allergies(data["ingredients"])
-            new_data["ingredients"] = data["ingredients"]
-        print("===", new_data)
-        print(server_client.collections['WaitlistProducts'].documents.create(new_data))
+            server_client.collections["WaitlistProducts"].documents.create(new_data)
 
-        dict = {"message": "success","data":new_data}
-        return JsonResponse(dict)
+            dict_response = {"message": "success", "data": new_data}
+            return JsonResponse(dict_response)
+
+        except KeyError as e:
+            return JsonResponse({"error": f"KeyError: {e}"}, status=400)
+
+        except FileNotFoundError as e:
+            return JsonResponse({"error": f"FileNotFoundError: {e}"}, status=500)
+
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
