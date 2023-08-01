dbe2a62829549802da97d448cbc8a110fa38cc28
Dhanapandi M <dhanapandi.m007@gmail.com>
2023-08-01 13:28:39 +0530

    reverts the change

--- off-nutrition-table-extractor/settings.py
+++ off-nutrition-table-extractor/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-a&48ca=r!lz)c^b+k*rxpn%-z+(boe1+-zz7*a^8&fyh33q@-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
