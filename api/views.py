d558f857026582e44282e3839020670e5e7d8d60
Dhanapandi <dhanapandi.m007@gmail.com>
2023-11-21 14:04:09 +0530

    fixes add product to Db not working issue

--- api/views.py
+++ api/views.py
@@ -1,11 +1,12 @@
+
 from detection import detect_all
 import base64
 import io
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from ingredient_extractor.vision import extract_ingredients_from_image
-# from process_product.client import server_client
-# from .allergies import check_allergies,get_top_three,Dv_values
+from process_product.client import server_client
+from .allergies import check_allergies,get_top_three,Dv_values
 from .models import *
 from .forms import UploadFileForm
 import cv2
@@ -63,129 +64,129 @@ def health(request):
 
 
 
-# @csrf_exempt
-# def ingredientExtract(request):
-#     if request.method == 'POST':
-#         try:
-#             if 'image' not in request.FILES:
-#                 return JsonResponse({"error": "No 'image' file provided."}, status=400)
+@csrf_exempt
+def ingredientExtract(request):
+    if request.method == 'POST':
+        try:
+            if 'image' not in request.FILES:
+                return JsonResponse({"error": "No 'image' file provided."}, status=400)
 
-#             new_file = UploadFile(file=request.FILES['image'])
-#             new_file.save()
+            new_file = UploadFile(file=request.FILES['image'])
+            new_file.save()
 
-#             name = new_file.file.name
-#             file_path = new_file.file.path
+            name = new_file.file.name
+            file_path = new_file.file.path
 
-#             with io.open(file_path, 'rb') as img:
-#                 file_data = img.read()
-#                 file_base64 = base64.b64encode(file_data).decode('utf-8')
+            with io.open(file_path, 'rb') as img:
+                file_data = img.read()
+                file_base64 = base64.b64encode(file_data).decode('utf-8')
 
-#                 response = extract_ingredients_from_image(file_base64)
-#                 dict_response = {"ingredients": response}
+                response = extract_ingredients_from_image(file_base64)
+                dict_response = {"ingredients": response}
 
-#             os.remove(file_path)
+            os.remove(file_path)
 
-#             return JsonResponse(dict_response)
+            return JsonResponse(dict_response)
 
-#         except Exception as e:
-#             return JsonResponse({"error": str(e)}, status=500)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
 
 
-# @csrf_exempt
-# def process_new_product(request):
-    # if request.method == 'POST':
-    #     try:
+@csrf_exempt
+def process_new_product(request):
+    if request.method == 'POST':
+        try:
             
-    #         csv_filename = "api/Dataset analysis.csv"
-    #         all_nutrients = []
-    #         all_nutrients_mapping = {}
-    #         unit_mapping = {}
+            csv_filename = "api/Dataset analysis.csv"
+            all_nutrients = []
+            all_nutrients_mapping = {}
+            unit_mapping = {}
 
-    #         body_unicode = request.body.decode('utf-8')
-    #         body = json.loads(body_unicode)
+            body_unicode = request.body.decode('utf-8')
+            body = json.loads(body_unicode)
 
-    #         data = body["data"]
-    #         nutrients = {"macros": {}, "vitamins": {}, "minerals": {}}
-    #         with open(csv_filename) as f:
-    #             reader = csv.DictReader(f)
+            data = body["data"]
+            nutrients = {"macros": {}, "vitamins": {}, "minerals": {}}
+            with open(csv_filename) as f:
+                reader = csv.DictReader(f)
 
-    #             for row in reader:
-    #                 name = row["name"].strip().lower()
-    #                 all_nutrients.append(name)
-    #                 all_nutrients_mapping[name] = {
-    #                     "name": name,
-    #                     "new_name": row["new_name"].strip(),
-    #                     "category": row["category"].strip(),
-    #                     # "new_unit": row["new_unit"],
-    #                 }
-    #                 unit_mapping[row["new_name"].strip()] = {
-    #                     "category": row["category"].strip(),
-    #                     # "new_unit": row["new_unit"],
-    #                 }
+                for row in reader:
+                    name = row["name"].strip().lower()
+                    all_nutrients.append(name)
+                    all_nutrients_mapping[name] = {
+                        "name": name,
+                        "new_name": row["new_name"].strip(),
+                        "category": row["category"].strip(),
+                        # "new_unit": row["new_unit"],
+                    }
+                    unit_mapping[row["new_name"].strip()] = {
+                        "category": row["category"].strip(),
+                        # "new_unit": row["new_unit"],
+                    }
 
-    #         for record in data["nutrients"]:
-    #             result = all_nutrients_mapping.get(record["name"].lower())
-    #             key = record["name"].strip()
+            for record in data["nutrients"]:
+                result = all_nutrients_mapping.get(record["name"].lower())
+                key = record["name"].strip()
 
-    #             if result is not None:
-    #                 value = record["amount"]
+                if result is not None:
+                    value = record["amount"]
 
-    #                 if (
-    #                     record["unitName"].strip() == "%"
-    #                     and Dv_values.get(result["new_name"])
-    #                 ):
-    #                     value = (
-    #                         Dv_values.get(result["new_name"]).get("amount")
-    #                         * value
-    #                         / 100
-    #                     )
-    #                     nutrients[result["category"]][result["new_name"]] = {
-    #                         "value": value,
-    #                         "unit": Dv_values.get(result["new_name"]).get("unit"),
-    #                     }
-    #                 else:
-    #                     nutrients[result["category"]][result["new_name"]] = {
-    #                         "value": value,
-    #                         "unit": record["unitName"],
-    #                     }
+                    if (
+                        record["unitName"].strip() == "%"
+                        and Dv_values.get(result["new_name"])
+                    ):
+                        value = (
+                            Dv_values.get(result["new_name"]).get("amount")
+                            * value
+                            / 100
+                        )
+                        nutrients[result["category"]][result["new_name"]] = {
+                            "value": value,
+                            "unit": Dv_values.get(result["new_name"]).get("unit"),
+                        }
+                    else:
+                        nutrients[result["category"]][result["new_name"]] = {
+                            "value": value,
+                            "unit": record["unitName"],
+                        }
 
-    #         top_three = get_top_three(nutrients["vitamins"] | nutrients["minerals"])
-    #         # generate a random 6-digit number between 100000 and 999999 (inclusive)
-    #         random_number = random.randint(100000, 999999)
+            top_three = get_top_three(nutrients["vitamins"] | nutrients["minerals"])
+            # generate a random 6-digit number between 100000 and 999999 (inclusive)
+            random_number = random.randint(100000, 999999)
 
-    #         new_data = {
-    #             "dataType": "Branded",
-    #             "description": data["description"],
-    #             "brandOwner": data["brandOwner"],
-    #             "gtinUpc": data["gtinUpc"],
-    #             "nutrients": str(json.dumps(nutrients)),
-    #             "brandName": data["brandName"],
-    #             "addedBy": data["uid"],
-    #             "topThree": top_three,
-    #             "descriptionLength": len(data["description"]),
-    #             "fdcId": data["uid"] + "-" + str(random_number),
-    #         }
-    #         if "servingSize" in data:
-    #             new_data["servingSize"] = data["servingSize"]
-    #         if "servingSizeUnit" in data:
-    #             new_data["servingSizeUnit"] = data["servingSizeUnit"]
-    #         if "packageWeight" in data:
-    #             new_data["packageWeight"] = data["packageWeight"]
-    #         if "ingredients" in data:
-    #             new_data["allergyInformation"] = check_allergies(data["ingredients"])
-    #             new_data["ingredients"] = data["ingredients"]
+            new_data = {
+                "dataType": "Branded",
+                "description": data["description"],
+                "brandOwner": data["brandOwner"],
+                "gtinUpc": data["gtinUpc"],
+                "nutrients": str(json.dumps(nutrients)),
+                "brandName": data["brandName"],
+                "addedBy": data["uid"],
+                "topThree": top_three,
+                "descriptionLength": len(data["description"]),
+                "fdcId": data["uid"] + "-" + str(random_number),
+            }
+            if "servingSize" in data:
+                new_data["servingSize"] = data["servingSize"]
+            if "servingSizeUnit" in data:
+                new_data["servingSizeUnit"] = data["servingSizeUnit"]
+            if "packageWeight" in data:
+                new_data["packageWeight"] = data["packageWeight"]
+            if "ingredients" in data:
+                new_data["allergyInformation"] = check_allergies(data["ingredients"])
+                new_data["ingredients"] = data["ingredients"]
 
-    #         server_client.collections["WaitlistProducts"].documents.create(new_data)
+            server_client.collections["WaitlistProducts"].documents.create(new_data)
 
-    #         dict_response = {"message": "success", "data": new_data}
-    #         return JsonResponse(dict_response)
+            dict_response = {"message": "success", "data": new_data}
+            return JsonResponse(dict_response)
 
-    #     except KeyError as e:
-    #         return JsonResponse({"error": f"KeyError: {e}"}, status=400)
+        except KeyError as e:
+            return JsonResponse({"error": f"KeyError: {e}"}, status=400)
 
-    #     except FileNotFoundError as e:
-    #         return JsonResponse({"error": f"FileNotFoundError: {e}"}, status=500)
+        except FileNotFoundError as e:
+            return JsonResponse({"error": f"FileNotFoundError: {e}"}, status=500)
 
-    #     except Exception as e:
-    #         return JsonResponse({"error": str(e)}, status=500)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
 
--- process_product/client.py
+++ process_product/client.py
@@ -1,23 +1,23 @@
-# import typesense
+import typesense
 
-# from dotenv import load_dotenv
-# import os
+from dotenv import load_dotenv
+import os
 
-# load_dotenv()
-# TYPESENSE_API_KEY = os.getenv("TYPESENSE_API_KEY")
-# schema = {
-#     "name": "WaitlistProducts",
-#     "fields": [
-#         {"name": "fdcId", "type": "string"},
-#          {"name": "addedBy", "type": "string"},
-#         {"name": "topThree", "type": "string[]"},
-#         {"name": "dataType", "type": "string","sort":True},
-#         {"name": "description", "type": "string", "token_separators": [",", "."]},
+load_dotenv()
+TYPESENSE_API_KEY = os.getenv("TYPESENSE_API_KEY")
+schema = {
+    "name": "WaitlistProducts",
+    "fields": [
+        {"name": "fdcId", "type": "string"},
+         {"name": "addedBy", "type": "string"},
+        {"name": "topThree", "type": "string[]"},
+        {"name": "dataType", "type": "string","sort":True},
+        {"name": "description", "type": "string", "token_separators": [",", "."]},
 
         
-#         {"name": ".*", "type": "auto"},
-#     ],
-# }
+        {"name": ".*", "type": "auto"},
+    ],
+}
 # local_client = typesense.Client(
 #     {
 #         "nodes": [
@@ -33,20 +33,20 @@
 #         "connection_timeout_seconds": 200,
 #     }
 # )
-# server_client = typesense.Client(
-#     {
-#         "nodes": [
-#             {
+server_client = typesense.Client(
+    {
+        "nodes": [
+            {
                 
-#                 'host': 'localhost',  # For Typesense Cloud use xxx.a1.typesense.net
-#                 "port": "8108",  # For Typesense Cloud use 443
-#                 "protocol": "http",  # For Typesense Cloud use https
-#             }
-#         ],
+                'host': 'localhost',  # For Typesense Cloud use xxx.a1.typesense.net
+                "port": "8108",  # For Typesense Cloud use 443
+                "protocol": "http",  # For Typesense Cloud use https
+            }
+        ],
    
-#           'api_key': TYPESENSE_API_KEY,
-#         "connection_timeout_seconds": 200,
-#     }
-# )
+          'api_key': TYPESENSE_API_KEY,
+        "connection_timeout_seconds": 200,
+    }
+)
 
 
